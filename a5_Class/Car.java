af44a464868af56389b20cd141cc658d49a49878
asa9874 <pjkpjk1129@gmail.com>
2024-05-15 16:15:49 +0900

    class

--- a5_Class/Car.java
+++ a5_Class/Car.java
@@ -1,9 +1,31 @@
 package a5_Class;
 
 public class Car {
-    
+    String name="슈퍼멋진차";
+    int tire_num=4;
 }
 
-class Tire{
+class Car2{
+    String name="기본차";
+    int tire_num;
+    int people;
+    Car2(String name){
+        this(name,4,4);
+    } 
+
+    Car2(String name, int tire_num){
+        this(name,tire_num,4);
+    } 
+
+    Car2(String name, int tire_num, int people){
+        this.name=name;
+        this.tire_num=tire_num;
+        this.people=people;
+    } 
+
+
+
+
+    
 
 }
--- a5_Class/a5_class.java
+++ a5_Class/a5_class.java
@@ -2,8 +2,10 @@ package a5_Class;
 
 public class a5_class {
     public static void main(String[] args){
-        Car car1= new Car();
-        Car car2= new Car();
-        System.out.println(car1==car2);
+
+        Car2 car2= new Car2("붕붕이");
+        Car2 car3= new Car2("붕붕삼",6);
+        System.out.println(car2.tire_num);
+        System.out.println(car3.tire_num);
     }
 }
