f3fa3757d28c9bd589a922bae1141e7efbda46a9
asa9874 <pjkpjk1129@gmail.com>
2024-05-15 16:45:01 +0900

    static

---
+++ a7_Class_instance/a7_class.java
@@ -0,0 +1,23 @@
+package a7_Class_instance;
+
+public class a7_class {
+    public static void main(String[] args){
+        Car car1=new Car("방방이");
+        car1.setName("빙빙이");
+        System.out.println(car1.name); 
+        System.out.println(car1.people);       
+        System.out.println(Car.tire_num);
+        
+
+        Car2 car2=Car2.getInstance();
+        Car2 car3=Car2.getInstance();
+        System.out.println(car2==car3);
+
+
+
+
+    }
+}
+
+
+
