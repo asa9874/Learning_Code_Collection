000bc8aff03874f6572d73870249c844ebdb834d
asa9874 <pjkpjk1129@gmail.com>
2024-05-15 22:39:24 +0900

    gettersetter

---
+++ a8_Class_gettersetter/Car.java
@@ -0,0 +1,14 @@
+package a8_Class_gettersetter;
+
+public class Car {
+    private int speed;
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void setSpeed(int speed){
+        if(speed>0){
+            this.speed=speed;
+        }
+    }
+}
