f214d4b023768a8ecf2e349d391a30fb22db9833
asa9874 <pjkpjk1129@gmail.com>
2024-05-17 18:02:11 +0900

    interface

---
+++ a12_Interface/RemoteCotrol.java
@@ -0,0 +1,27 @@
+package a12_Interface;
+
+public interface RemoteCotrol {
+    public int Max_VOLUME=10;
+    public int Min_VOLUME=0;
+
+    public void Turn_on();
+    public void Turn_off();
+}
+
+interface Internet{
+    public void connect_internet();
+}
+
+class Television implements RemoteCotrol,Internet{
+    @Override
+    public void Turn_on() {
+        System.out.println("TV ON");
+    }
+
+    public void Turn_off(){
+        System.out.println("TV OFF");
+    }
+    public void connect_internet(){
+        System.out.println("INTERNET CONNECTED");
+    }
+}
---
+++ a12_Interface/a12.java
@@ -0,0 +1,9 @@
+package a12_Interface;
+
+public class a12 {
+    public static void main(String[] args){
+        RemoteCotrol remote1;
+        remote1= new Television();
+    }
+
+}
