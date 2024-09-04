484a1da90aea6c3e8b137c10018abea72332af01
asa9874 <pjkpjk1129@gmail.com>
2024-09-04 17:22:17 +0900

    express

--- Express/index.js
+++ Express/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const template = require('./lib/template.js');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+var qs = require('querystring');
+
 // 포트 설정
 const port = 3000;
 
@@ -16,13 +20,72 @@ app.get('/', (request, response) => {
       `<h2>${title}</h2>${description}`,
       `<a href="/create">create</a>`
     );
-    response.end(html);
+    response.send(html);
+  });
+}); 
+
+
+
+//상세 페이지
+app.get('/page/:pageId', (request, response) => {
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
   });
 });
 
-app.get('/page/:pageId', (request, response) => {
-  response.send(request.params)
-});
+
+app.get('/create',function(request,response){
+  fs.readdir('./data', function(error, filelist){
+    var title = 'WEB - create';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    response.send(html);
+  });
+})
+
+app.post('/create_process',function(request,response){
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      })
+  });
+})
 
 
 // 서버 시작
