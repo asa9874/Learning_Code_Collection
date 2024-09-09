835fd2c3530d5c8a32badd7a25c66a3f56e2ec9d
asa9874 <pjkpjk1129@gmail.com>
2024-09-09 23:19:11 +0900

    Java HashMap

---
+++ JAVA/hashmap.java
@@ -0,0 +1,22 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<String,String> map1 = new HashMap<>();
+        map1.put("asd", "qqq");
+        map1.get("asd");
+        map1.remove("asd");
+        if(map1.containsKey("asd")){};
+        if(map1.containsKey("qqq")){};
+        map1.size();
+
+        if(map1.isEmpty()){};
+        map1.clear();
+        Set<String> keys= map1.keySet();
+        Collection<String> values = map1.values();
+        map1.putIfAbsent("orange", "asd");
+        map1.replace("orange", "asd");
+    }
+}
