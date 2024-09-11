fc73381937e0f02595456ea017b7be79ef1eb5ce
asa9874 <pjkpjk1129@gmail.com>
2024-09-11 21:48:52 +0900

    Express 쿠키

---
+++ Express/Express2/app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('*',(req,res,next)=>{
+  res.cookie('Coooookieee','testval',{httpOnly:true});
+  res.cookie('longgggg','testval',{maxAge: 900000, httpOnly:true});
+  res.cookie('secure','secure',{ secure: true });
+  res.cookie('name', 'value', { path: '/user' });
+  res.cookie('name', 'value', { domain: '.example.com' });
+  const cookies=req.cookies;
+  console.log(cookies);
+  next()
+})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
