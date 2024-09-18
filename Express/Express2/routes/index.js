08d918d26dc7dfea5abd3e4d2d53d7520adbd9b2
asa9874 <pjkpjk1129@gmail.com>
2024-09-18 20:38:18 +0900

    Express 세션

--- Express/Express2/routes/index.js
+++ Express/Express2/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var express = require('express')
+var parseurl = require('parseurl')
+var session = require('express-session')
+var FileStore = require('session-file-store')(session);
+
+router.use(session({
+  store: new FileStore(),
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+router.use(function (req, res, next) {
+  if (!req.session.views) {
+    req.session.views = {}
+  }
+
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+
+  // count the views
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+
+  next()
+})
+
+router.get('/foo', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+})
+
+router.get('/bar', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+})
 
 module.exports = router;
