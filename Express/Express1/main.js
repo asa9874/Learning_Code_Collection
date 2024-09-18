e7c80079e23b96dd63014ae865d7b0cb6bd8b902
asa9874 <pjkpjk1129@gmail.com>
2024-09-18 21:39:45 +0900

    Express Session & Auth 6.3. 인증구현

--- Express/Express1/main.js
+++ Express/Express1/main.js
@@ -5,13 +5,24 @@ var bodyParser = require('body-parser');
 var compression = require('compression')
 var helmet=require('helmet')
 app.use(helmet());
+var session = require('express-session');
+var FileStore = require('session-file-store')(session)
 
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
+var authRouter = require('./routes/auth');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+
+app.use(session({
+  store: new FileStore(),
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.get('*', function(request, response, next){
   fs.readdir('./data', function(error, filelist){
     request.list = filelist;
@@ -21,6 +32,7 @@ app.get('*', function(request, response, next){
  
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
+app.use('/auth', authRouter);
  
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
