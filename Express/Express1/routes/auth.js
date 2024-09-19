ae167528b7ce80a85da112f91e41a8fb7af48ff6
asa9874 <pjkpjk1129@gmail.com>
2024-09-19 20:35:18 +0900

    Express Session & Auth

--- Express/Express1/routes/auth.js
+++ Express/Express1/routes/auth.js
@@ -13,13 +13,6 @@ var authData={
 }
 
 
-
-
-
-
-
-
-
 router.get('/login', function (request, response) {
   var title = 'WEB - login';
   var list = template.list(request.list);
@@ -39,17 +32,33 @@ router.post('/login_process', function (request, response) {
   var post = request.body;
   var email = post.email;
   var password = post.pwd;
-  if(email===authData.email && password===authData.password){
-    request.session.is_logined=true;
-    request.session.nickname=authData.nickname;
-    response.redirect(`/`);
+  if(email === authData.email && password === authData.password){
+    request.session.is_logined = true;
+    request.session.nickname = authData.nickname;
+    request.session.save(function(){
+      response.redirect(`/`);
+    });
+  } else {
+    response.send('Who?');
   }
-  else{
-    response.send("???");
-  }
-
-  
 });
+
+router.get('/logout', function (request, response) {
+  request.session.destroy(function(err){
+    response.redirect('/');
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
